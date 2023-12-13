919023c946943c6916994524fe72af5df44bf805
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-14 04:28:10 +0600

    create blog-post and side-bar component

--- Angular-1/src/app/app.component.ts
+++ Angular-1/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule, RouterOutlet, BlogPostComponent, SideBarComponent]
 })
 export class AppComponent {
-  title = 'Angular-1';
+  title = 'Angular';
 }

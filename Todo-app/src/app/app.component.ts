f70e8ec6171a29c2b51c16b3a74b01c82783f559
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-15 21:25:06 +0600

    apply CURD in the to do app

--- Todo-app/src/app/app.component.ts
+++ Todo-app/src/app/app.component.ts
@@ -1,14 +1,52 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Todo } from './todoModel';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Todo-app';
+  title = 'Todo List';
+
+  todoList: Todo[] = [];
+
+  todo: Todo = {
+    id: null,
+    title: ""
+  }
+
+  addTodo(): void {
+
+    if (this.todo.id) {
+
+      this.todoList = this.todoList.map(item => {
+        if (item.id === this.todo.id) {
+          item.title = this.todo.title;
+        }
+        return item;
+      })
+    } else {
+      this.todo.id = Date.now();
+      this.todoList.push({ ...this.todo });
+    }
+    this.todo = {
+      id: null,
+      title: ""
+    }
+    console.log(this.todoList);
+  }
+
+  editTodo(item: Todo): void {
+    this.todo = { ...item };
+  }
+
+  deleteTodo(id: any): void {
+    this.todoList = this.todoList.filter(item => item.id != id);
+  }
 }

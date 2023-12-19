6dfc77e4819fdb675999ef366664342d9ed9d550
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-20 00:20:50 +0600

    create  ccalculateAge pipe and use it for better perform

---
+++ pipe/src/app/calculate-age.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'calculateAge',
+  standalone: true
+})
+export class CalculateAgePipe implements PipeTransform {
+
+  transform(date: string, ...args: unknown[]): number {
+    return new Date().getFullYear() - new Date(date).getFullYear();
+  }
+
+}

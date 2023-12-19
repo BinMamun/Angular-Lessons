076a261e094a489f43ba991cf4643d56cdd62695
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-20 00:16:52 +0600

    use of pipe

--- pipe/src/app/app.component.ts
+++ pipe/src/app/app.component.ts
@@ -10,5 +10,35 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'pipe';
+
+  users = [
+    {
+      name: "Afsan Reza",
+      date_of_birth: "1998-12-25",
+      created_date: "2013-07-25"
+    },
+    {
+      name: "Akash Ahmed",
+      date_of_birth: "2000-12-25",
+      created_date: "2015-03-25"
+    },
+    {
+      name: "Tanim Mahbir",
+      date_of_birth: "2002-12-25",
+      created_date: "2023-05-25"
+    }
+  ]
+
+  adduser() {
+    this.users.push({
+      name: "Konan Doyel",
+      date_of_birth: "1978",
+      created_date: "2020-07-15"
+    });
+  }
+
+  calculateAge(date: string): number {
+    console.log(date);
+    return new Date().getFullYear() - new Date(date).getFullYear();
+  }
 }
